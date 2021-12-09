8631be7b50266b84253dabbb46d92bab1d556850
Antonio Murdaca <runcom@linux.com>
2021-12-09 12:12:37 +0100

    relicense to MIT
    Signed-off-by: Antonio Murdaca <runcom@linux.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
 // Copyright 2020 Patrick Uiterwijk
 //
-// Licensed under the EUPL-1.2-or-later
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Licensed under the MIT license
 
 use std::convert::{TryFrom, TryInto};
 use std::env;
